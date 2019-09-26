d590adced245e111df99e2c77e1b13efb2f09574
Nick <nick.moi@gmail.com>
2019-09-26 01:31:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -44,7 +44,7 @@ def hit?(current_total)
   elsif input == "h" 
     new_total = current_total + deal_card
     return new_total
-  elsif input != ("h" || "s")
+  elsif input != "h" || "s"
     invalid_command
     return current_total
   end
